2099354504fb291a05ed9d79b9e99bf9b41bd7f0
Morteza <seyedmorteza2400@gmail.com>
2023-01-20 22:32:09 +0330

    add features

--- Library.java
+++ Library.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Person{
     String name;
-    Book[] booksThatBorrowed = new Book[100];
+    String[] booksThatBorrowed = new String[100];
     int numberOfBorrowing;
     boolean isInLibrary;
 
@@ -10,10 +10,18 @@ class Person{
         this.name=name;
         numberOfBorrowing=0;
     }
-    void borrow(){
+    void borrow(String bookName){
+        booksThatBorrowed[numberOfBorrowing]=bookName;
         numberOfBorrowing++;
+        
     }
-    void returnBook(){
+    void returnBook(String bookName){
+        for (int i=0; i<numberOfBorrowing; i++){
+            if (booksThatBorrowed[i].equals(bookName)){
+                booksThatBorrowed[i]=booksThatBorrowed[numberOfBorrowing-1];
+                break;
+            }
+        }
         numberOfBorrowing--;
     }
 
@@ -24,8 +32,8 @@ class Person{
 
 class Book{
     String name = new String();
+    String[] personsThatBorrowedThis = new String[100];
     int count;
-    int numberOfBorrowing;
     int borrowed;
     
     Book(String name){
@@ -38,7 +46,18 @@ class Book{
     void remove(){
         count--;
     }
-    void borrow(){
+
+    void returnBook(String personName){
+        for (int i=0; i<borrowed; i++){
+            if (personsThatBorrowedThis[i].equals(personName)){
+                personsThatBorrowedThis[i]=personsThatBorrowedThis[borrowed-1];
+                break;
+            }
+        }
+        borrowed--;
+    }
+    void borrow(String personName){
+        personsThatBorrowedThis[borrowed]=personName;
         borrowed++;
     }
 }
@@ -182,22 +201,45 @@ class LibrariesPeople{
             }
             temp2=temp2.children[index];
         }
-        if (temp2.book.count==0){
+        if (temp2.book.count - temp2.book.borrowed == 0){
             System.out.println("The book is not available");
             return;
         }
-        temp.person.borrow();
+        temp.person.borrow(temp2.book.name);
         temp2.book.borrow();
-        temp.person.booksThatBorrowed[temp.person.numberOfBorrowing-1]=temp2.book;
+        // assign the book to the person
+        
+    }
+    void returnBook(String personName, String bookName){
+        PersonNode temp = root;
+        for (int i=0; i<personName.length(); i++){
+            int index = personName.charAt(i)-65;
+            if (temp.children[index]==null){
+                temp.children[index]=new PersonNode(personName.charAt(i),temp);
+            }
+            temp=temp.children[index];
+        }
+        if (temp.person==null){
+            temp.person=new Person(personName);
+        }
+        if (!temp.person.isInLibrary){
+            System.out.println("The person is not in the library");
+            return;
+        }
+        BookNode temp2 = librariesBooks.root;
+        for (int i=0; i<bookName.length(); i++){
+            int index = bookName.charAt(i)-65;
+            if (temp2.children[index]==null){
+                temp2.children[index]=new BookNode(bookName.charAt(i),temp2);
+            }
+            temp2=temp2.children[index];
+        }
+        if (temp2.book.count - temp2.book.borrowed == 0){
+            System.out.println("The book is not available");
+            return;
+        }
+        temp.person.returnBook();
+        temp2.book.borrow();
     }
 
-
-
 }
-
-
-
-
-
-
-
